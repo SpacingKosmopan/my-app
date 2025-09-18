62c84c41883d5643114555ec253565a9f3c2000d
Spacing Kosmos <spacingkosmopan.mapy@gmail.com>
2025-09-18 08:37:48 +0200

    Initial identification commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./assets/Task";
+
+function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    const newToDoList = [...toDoList, task];
+    setToDoList(newToDoList);
+  };
+
+  const deleteTask = (id) => {
+    setToDoList(toDoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setToDoList(
+      toDoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  return (
+    <>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {toDoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+              completed={task.completed}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
